cf217c46aa76b4acd8cf225b01ea98b61bf5a7fe
Eduardo Fran <franeduardo305@gmail.com>
2026-02-16 12:29:39 +0000

    Fix dim_vendors code

--- models/marts/dim_vendors.sql
+++ models/marts/dim_vendors.sql
@@ -1,11 +1,13 @@
-with trips_inioned as {
+with trips_unioned as (
     select * from {{ ref('int_trips_unioned') }}
-},
+),
 
 vendors as (
-    select
-        distinct vendor_id,
+    select distinct
+        vendor_id,
         {{ get_vendor_names('vendor_id') }} as vendor_name
-    from trips_inioned
-
+    from trips_unioned
 )
+
+select *
+from vendors
